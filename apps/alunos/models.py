6fcbbb8eac172e24cb8bb1a7e74ff949082b3498
anacanestraro <ana.canestraro07@gmail.com>
2026-02-02 18:49:14 -0300

    Related #10: Adicionando model de aluno

--- apps/alunos/models.py
+++ apps/alunos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Aluno(models.Model):
+    STATUS_CHOICES = [
+        ('ativo', 'Ativo'),
+        ('trancado', 'Trancado'),
+        ('formado', 'Cancelado'),
+    ]
+
+    usuario = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name = 'aluno')
+    matricula = models.CharField(max_length = 20, unique = True)
+    cpf = models.CharField(max_length = 11, unique = True)
+    data_nascimento = models.DateField()
+    data_ingresso = models.DateField(auto_now_add = True)
+    status = models.CharField(max_length = 10, choices = STATUS_CHOICES, default = 'ativo')
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        verbose_name = 'Aluno'
+        verbose_name_plural = 'Alunos'
+    
+    def __str__(self):
+        return f"{self.matricula} - {self.usuario.get_full_name()}"
